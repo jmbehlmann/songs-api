29cf611a61be4a7959cba6afd3f0af34b0f84ac2
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2023-12-06 13:11:33 -0600

    created index and show route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+# get all
+get "/songs" => "songs#index"
+# show 1 song by id
+get "/songs/:id" => "songs#show"
 end
